53a0b066beb058c94ef9b3c0a3c15829f20a0b09
edielson-assis <edielsonnice@hotmail.com>
2026-07-16 14:44:43 -0300

    refactor: removido validações das entidades e centralizada nos DTOs

--- models/Person.cs
+++ models/Person.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ControleGastos.Api.models;
@@ -6,14 +5,10 @@ namespace ControleGastos.Api.models;
 [Table("People")]
 public class Person
 {
-    [Key]
     public long Id { get; set; }
 
-    [Required]
-    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
 
-    [Range(0, 120)]
     public int Age { get; set; }
 
     public ICollection<Transaction> Transactions { get; set; }
--- models/Transaction.cs
+++ models/Transaction.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using ControleGastos.Api.models.enums;
 
@@ -7,20 +6,14 @@ namespace ControleGastos.Api.models;
 [Table("Transactions")]
 public class Transaction
 {
-    [Key]
     public long Id { get; set; }
 
-    [Required]
-    [MaxLength(200)]
     public string Description { get; set; } = string.Empty;
 
-    [Required]
     public decimal Amount { get; set; }
 
-    [Required]
     public TransactionType Type { get; set; }
 
-    [ForeignKey(nameof(Person))]
     public long PersonId { get; set; }
 
     public Person? Person { get; set; }
