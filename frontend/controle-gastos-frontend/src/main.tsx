2792602dca95ac82a3ca7d6f068abc5cd4556380
edielson-assis <edielsonnice@hotmail.com>
2026-07-18 16:27:46 -0300

    refactor: reorganizado estrutura de estilos do frontend

--- frontend/controle-gastos-frontend/src/main.tsx
+++ frontend/controle-gastos-frontend/src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+
+import "./styles/reset.css";
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/layout.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
