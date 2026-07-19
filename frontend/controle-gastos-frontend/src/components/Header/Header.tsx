7ed1475e47d3bb459a22204127e1bf5b46f1a262
edielson-assis <edielsonnice@hotmail.com>
2026-07-18 23:39:53 -0300

    style: atualizado estilizacao no header

--- frontend/controle-gastos-frontend/src/components/Header/Header.tsx
+++ frontend/controle-gastos-frontend/src/components/Header/Header.tsx
@@ -4,7 +4,6 @@ function Header() {
     return (
         <header className="header">
             <h1>Controle de Gastos</h1>
-            <p>Gerencie receitas e despesas de forma simples.</p>
         </header>
     );
 }
